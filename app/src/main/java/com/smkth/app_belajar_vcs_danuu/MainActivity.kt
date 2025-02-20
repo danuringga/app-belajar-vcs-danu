bc77e7d23167f50f7fb071bff62cb00fa9cdd9fb
danuringga <mocdanuringga.com>
2025-02-20 11:15:56 +0700

    haloo

---
+++ app/src/main/java/com/smkth/app_belajar_vcs_danuu/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.smkth.app_belajar_vcs_danuu
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.inputmethod.EditorInfoCompat
+import com.smkth.app_belajar_vcs_danuu.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.btn.setOnClickListener{
+            Toast.makeText(this, "Halooo", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
